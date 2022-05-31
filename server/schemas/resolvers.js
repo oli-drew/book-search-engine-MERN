84ebb1792d51bf2b3f1c131a2acfcc7f28601966
Oliver Drew <oli.drew92@gmail.com>
2022-05-31 15:09:24 +0100

    Updated server.js and resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,19 @@
 const { Book, User } = require("../models");
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
-    user: async () => {
-      return User.find({});
+    user: async (parent, { username }) => {
+      return User.findOne({ username });
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
     },
   },
 };
