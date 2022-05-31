3ab0c6239906cd9de6d4f73273ab53be20ac7f7e
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-05-31 15:56:07 +0100

    Updated resolvers with add and remove books

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,6 +7,12 @@ const resolvers = {
     user: async (parent, { username }) => {
       return User.findOne({ username });
     },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate("savedBooks");
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
   },
 
   Mutation: {
@@ -14,6 +20,43 @@ const resolvers = {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      // Check if user exists
+      if (!user) {
+        throw new AuthenticationError("No user found with this email address");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      // Check password is correct
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      // If email and password are correct, sign user into the application with a JWT
+      const token = signToken(user);
+      // Return an `Auth` object
+      return { token, user };
+    },
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        const books = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: book._id } },
+          { new: true }
+        );
+        return books;
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    deleteBook: async (parent, { book }) => {
+      if (context.user) {
+        const removeBook = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: book._id } }
+        );
+        return removeBook;
+      }
+      throw new AuthenticationError("You need to be logged in!");
     },
   },
 };
